9e210ed10df0e155b9ae7879c165bc2d41db56b8
Wisso124214 <luisdavidbustosnunez@gmail.com>
2024-07-01 21:47:07 -0400

    frontend

--- src/components/modalView/modalView.jsx
+++ src/components/modalView/modalView.jsx
@@ -7,21 +7,22 @@ const CustomModal = ({handleClick, title, fetchUrl, imageKey}) => {
 
   const pokemonDetails = useFetchPokemonDetails(fetchUrl);
   const imageUrl = pokemonDetails ? pokemonDetails.sprites[imageKey] : '';
+  function handleClick(){
+    console.log('open')
+  }
 
-
-    return (
-        <div className="modal">
-            <div className="modal-content">
-                <div className="modal-header">
-                    <h4>{title.toUpperCase()}</h4>
-                </div>
-                <div className="modal-body">
-                <img src={imageUrl} alt={title} className='modal-image'/>
-                    <CustomButton label="Open" onClick={handleClick} />
-                </div>
-            </div>
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-body" onClick={handleClick}>
+          <img src={imageUrl} alt={title} className='modal-image'/>
         </div>
-    );
+        <div className="modal-header">
+          <h4>{title.toUpperCase()}</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CustomModal;
