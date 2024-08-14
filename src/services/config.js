325ed59757dc2c0101fd362a4f237eb24173642d
Wisso124214 <luisdavidbustosnunez@gmail.com>
2024-08-13 22:27:05 -0400

    first commit

--- src/services/config.js
+++ src/services/config.js
@@ -1,8 +1,8 @@
 const idOpacity = 0.5;
 const quantBarStats = 15;
 const activeColor = true;
-const hideBarSide = true;
-const showHidden = true;
+const hideBarSide = false;
+const showHidden = false;
 const maxVerPerCol = 7;
 
 export { activeColor, idOpacity, hideBarSide, quantBarStats, showHidden, maxVerPerCol };
